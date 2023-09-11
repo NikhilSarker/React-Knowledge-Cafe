be12f5b31cbee967c2bed8e04f161b881901e7e5
Nikhil Sarker <100969232+NikhilSarker@users.noreply.github.com>
2023-09-11 09:07:09 -0400

    taiwind install and setup

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+
+
+const Header = () => {
+  return (
+    <div>
+      <h3 className="text-2xl">Knowledge Cafe</h3>
+      <img src="" alt="" />
+      
+    </div>
+  );
+};
+
+export default Header;
